3b30264f4e64795216bd19778160bca450876efc
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-07-05 23:05:11 +0300

    Tune

--- sorting/src/insertion.rs
+++ sorting/src/insertion.rs
@@ -12,21 +12,23 @@ pub fn sort<T, F: Fn(&T, &T) -> bool>(target: &mut [T], is_ord: F) {
 
 #[cfg(test)]
 mod tests {
+    use super::sort as do_sort;
+
     #[test]
     fn sort() {
         {
             let mut v: Vec<i32> = vec![];
-            super::sort(v.as_mut_slice(), |_: &i32, _: &i32| { false });
+            do_sort(v.as_mut_slice(), |_: _, _: _| { false });
             assert_eq!(v, []);
         }
         {
             let mut v = vec![2, 3, 1, 4];
-            super::sort(v.as_mut_slice(), |i, j| { i < j });
+            do_sort(v.as_mut_slice(), |i, j| { i < j });
             assert_eq!(v, [1, 2, 3, 4]);
         }
         {
             let mut v = vec![2, 3, 1, 4];
-            super::sort(v.as_mut_slice(), |i, j| { i > j });
+            do_sort(v.as_mut_slice(), |i, j| { i > j });
             assert_eq!(v, [4, 3, 2, 1]);
         }
     }
