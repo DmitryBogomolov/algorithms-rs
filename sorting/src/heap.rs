9989e5536c7b9d68c356d06eb9cc83d827657474
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-07-19 11:14:35 +0300

    Add 'heap' sort stub

---
+++ sorting/src/heap.rs
@@ -0,0 +1,5 @@
+// Sorts using *Heap sort* algorithm.
+// https://algs4.cs.princeton.edu/24pq/
+pub fn sort<T, F: FnMut(&T, &T) -> bool>(target: &mut [T], mut is_ord: F) {
+    // TODO
+}
--- sorting/src/lib.rs
+++ sorting/src/lib.rs
@@ -2,6 +2,7 @@ pub mod insertion;
 pub mod shell;
 pub mod merge;
 pub mod quick;
+pub mod heap;
 
 #[cfg(test)]
 mod test_data;
