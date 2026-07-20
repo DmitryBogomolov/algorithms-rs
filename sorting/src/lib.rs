fe4b1783b83ce3a7efb10e250601b69969469f44
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-07-20 11:11:18 +0300

    Update crate export

--- sorting/src/lib.rs
+++ sorting/src/lib.rs
@@ -1,8 +1,14 @@
-pub mod insertion;
-pub mod shell;
-pub mod merge;
-pub mod quick;
-pub mod heap;
+mod insertion;
+mod shell;
+mod merge;
+mod quick;
+mod heap;
+
+pub use insertion::sort as insertion;
+pub use shell::sort as shell;
+pub use merge::sort as merge;
+pub use quick::sort as quick;
+pub use heap::sort as heap;
 
 #[cfg(test)]
 mod test_data;
