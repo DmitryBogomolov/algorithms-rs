7a829af3502771eec89e99107d0680c20d990e73
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-07-31 12:30:05 +0300

    Add implementation

--- priorityqueue/src/lib.rs
+++ priorityqueue/src/lib.rs
@@ -1,26 +1,84 @@
-pub struct PriorityQueue<T> {
+// Implements *Priority Queue* container.
+// https://algs4.cs.princeton.edu/24pq/
+pub struct PriorityQueue<T, F: FnMut(&T, &T) -> bool> {
     items: Vec<T>,
+    is_ord: F,
 }
 
-impl<T> PriorityQueue<T> {
-    pub fn new() -> PriorityQueue<T> {
-        todo!("")
+impl<T, F: FnMut(&T, &T) -> bool> PriorityQueue<T, F> {
+    pub fn new(is_ord: F) -> PriorityQueue<T, F> {
+        PriorityQueue {
+            items: Vec::new(),
+            is_ord,
+        }
+    }
+
+    pub fn with_capacity(is_ord: F, capacity: usize) -> PriorityQueue<T, F> {
+        PriorityQueue { 
+            items: Vec::with_capacity(capacity),
+            is_ord,
+        }
     }
 
     pub fn size(&self) -> usize {
-        todo!("")
+        self.items.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.size() == 0
+    }
+
+    fn sink(&mut self, i: usize) {
+        let items = &mut self.items;
+        let len = items.len();
+        let is_ord = &mut self.is_ord;
+        let mut parent = i;
+        loop {
+            let mut child = 2 * parent + 1;
+            if child + 1 < len && is_ord(&items[child], &items[child + 1]) {
+                child += 1;
+            }
+            if child >= len || !is_ord(&items[parent], &items[child]) {
+                break;
+            }
+            items.swap(parent, child);
+            parent = child;
+        }
+    }
+
+    fn swim(&mut self, i: usize) {
+        let items = &mut self.items;
+        let is_ord = &mut self.is_ord;
+        let mut child = i;
+        while child > 0 {
+            let parent = (child - 1) / 2;
+            if !is_ord(&items[parent], &items[child]) {
+                break;
+            }
+            items.swap(parent, child);
+            child = parent;
+        }        
     }
 
     pub fn insert(&mut self, element: T) {
-        todo!("")
+        self.items.push(element);
+        self.swim(self.items.len() - 1);
     }
 
-    pub fn peek(&self) -> &T {
-        todo!("")
+    pub fn peek(&self) -> Option<&T> {
+        if self.items.is_empty() {
+            return None;
+        }
+        Some(&self.items[0])
     }
 
-    pub fn remove(&mut self) -> T {
-        todo!("")
+    pub fn remove(&mut self) -> Option<T> {
+        if self.items.is_empty() {
+            return None;
+        }
+        let element = self.items.swap_remove(0);
+        self.sink(0);
+        Some(element)
     }
 }
 
@@ -30,7 +88,66 @@ mod tests {
 
     #[test]
     fn empty() {
-        let pq = PriorityQueue::<()>::new();
+        let pq = PriorityQueue::<(), _>::new(|_, _| false);
+        assert_eq!(pq.is_empty(), true);
         assert_eq!(pq.size(), 0);
+        assert_eq!(pq.peek(), None);
+    }
+
+    #[test]
+    fn add() {
+        let mut pq = PriorityQueue::new(|a, b| a < b);
+        
+        pq.insert(4);
+        assert_eq!(pq.is_empty(), false);
+        assert_eq!(pq.size(), 1);
+        assert_eq!(pq.peek(), Some(&4));
+
+        pq.insert(7);
+        assert_eq!(pq.size(), 2);
+        assert_eq!(pq.peek(), Some(&7));
+
+        pq.insert(2);
+        assert_eq!(pq.size(), 3);
+        assert_eq!(pq.peek(), Some(&7));
+
+        pq.insert(7);
+        assert_eq!(pq.size(), 4);
+        assert_eq!(pq.peek(), Some(&7));
+
+        pq.insert(9);
+        assert_eq!(pq.size(), 5);
+        assert_eq!(pq.peek(), Some(&9));
+    }
+
+    #[test]
+    fn remove() {
+        let mut pq = PriorityQueue::new(|a, b| a > b);
+        pq.insert(4);
+        pq.insert(6);
+        pq.insert(4);
+        pq.insert(3);
+        pq.insert(8);
+
+        assert_eq!(pq.size(), 5);
+        assert_eq!(pq.peek(), Some(&3));
+
+        assert_eq!(pq.remove(), Some(3));
+        assert_eq!(pq.size(), 4);
+
+        assert_eq!(pq.remove(), Some(4));
+        assert_eq!(pq.size(), 3);
+
+        assert_eq!(pq.remove(), Some(4));
+        assert_eq!(pq.size(), 2);
+
+        assert_eq!(pq.remove(), Some(6));
+        assert_eq!(pq.size(), 1);
+
+        assert_eq!(pq.remove(), Some(8));
+        assert_eq!(pq.size(), 0);
+
+        assert_eq!(pq.is_empty(), true);
+        assert_eq!(pq.remove(), None);
     }
 }
