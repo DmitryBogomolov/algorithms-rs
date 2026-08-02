3034ad279a314d0f53a401c402ae6afda9bc962b
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-08-02 20:29:40 +0300

    Rename

--- priorityqueue/src/lib.rs
+++ priorityqueue/src/lib.rs
@@ -1,24 +1,24 @@
 // Implements *Priority Queue* container.
 // https://algs4.cs.princeton.edu/24pq/
 pub struct PriorityQueue<T, F: FnMut(&T, &T) -> bool> {
-    items: Vec<T>,
+    heap: Vec<T>,
     is_ord: F,
 }
 
 impl<T, F: FnMut(&T, &T) -> bool> PriorityQueue<T, F> {
     pub fn new(is_ord: F) -> Self {
         Self {
-            items: Vec::new(),
+            heap: Vec::new(),
             is_ord,
         }
     }
 
     pub fn reserve(&mut self, capacity: usize) {
-        self.items.reserve(capacity);
+        self.heap.reserve(capacity);
     }
 
     pub fn size(&self) -> usize {
-        self.items.len()
+        self.heap.len()
     }
 
     pub fn is_empty(&self) -> bool {
@@ -26,7 +26,7 @@ impl<T, F: FnMut(&T, &T) -> bool> PriorityQueue<T, F> {
     }
 
     fn sink(&mut self, i: usize) {
-        let items = &mut self.items;
+        let items = &mut self.heap;
         let len = items.len();
         let is_ord = &mut self.is_ord;
         let mut parent = i;
@@ -44,7 +44,7 @@ impl<T, F: FnMut(&T, &T) -> bool> PriorityQueue<T, F> {
     }
 
     fn swim(&mut self, i: usize) {
-        let items = &mut self.items;
+        let items = &mut self.heap;
         let is_ord = &mut self.is_ord;
         let mut child = i;
         while child > 0 {
@@ -58,29 +58,29 @@ impl<T, F: FnMut(&T, &T) -> bool> PriorityQueue<T, F> {
     }
 
     pub fn insert(&mut self, element: T) {
-        let last = self.items.len();
-        self.items.push(element);
+        let last = self.heap.len();
+        self.heap.push(element);
         self.swim(last);
     }
 
     pub fn peek(&self) -> Option<&T> {
-        if self.items.is_empty() {
+        if self.heap.is_empty() {
             return None;
         }
-        Some(&self.items[0])
+        Some(&self.heap[0])
     }
 
     pub fn remove(&mut self) -> Option<T> {
-        if self.items.is_empty() {
+        if self.heap.is_empty() {
             return None;
         }
-        let element = self.items.swap_remove(0);
+        let element = self.heap.swap_remove(0);
         self.sink(0);
         Some(element)
     }
 
     pub fn clear(&mut self) {
-        self.items.clear();
+        self.heap.clear();
     }
 }
 
