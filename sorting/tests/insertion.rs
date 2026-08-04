4340a922846fcdfd36c9ec2d7235bbd7ff159a58
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2026-08-04 23:39:37 +0300

    Updates (#6)
    * Add named file
    
    * Move all UnionFind tests to 'integration'
    
    * Move all sorting tests to 'integration'
    
    * Fix warnings

---
+++ sorting/tests/insertion.rs
@@ -0,0 +1,32 @@
+mod test_data;
+use sorting::insertion as do_sort;
+
+#[test]
+fn sort_empty() {
+    let mut arr = test_data::array();
+    let target = &mut arr[0..0];
+    do_sort(target, test_data::asc);
+    test_data::check_sorted(target, test_data::asc);
+}
+
+#[test]
+fn sort_one() {
+    let mut arr = test_data::array();
+    let target = &mut arr[0..1];
+    do_sort(target, test_data::asc);
+    test_data::check_sorted(target, test_data::asc);
+}
+
+#[test]
+fn sort_asc() {
+    let mut v = test_data::array();
+    do_sort(&mut v, test_data::asc);
+    test_data::check_sorted(&v, test_data::asc);
+}
+
+#[test]
+fn sort_dsc() {
+    let mut v = test_data::array();
+    do_sort(&mut v, test_data::dsc);
+    test_data::check_sorted(&v, test_data::dsc);
+}
---
+++ unionfind/src/unionfind.rs
@@ -0,0 +1,74 @@
+// Union-Find implementation.
+// https://algs4.cs.princeton.edu/15uf/
+pub struct UnionFind {
+    size: usize,
+    count: usize,
+    roots: Vec<usize>,
+    sizes: Vec<usize>,
+}
+
+impl UnionFind {
+    pub fn new(size: usize) -> UnionFind {
+        UnionFind {
+            size,
+            count: size,
+            roots: (0..size).collect(),
+            sizes: vec![1; size],
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn count(&self) -> usize {
+        self.count
+    }
+
+    fn validate(&self, i: usize) {
+        if i >= self.roots.len() {
+            panic!("out of range: {}", i)
+        }
+    }
+
+    fn is_root(&self, i: usize) -> bool {
+        i == self.roots[i]
+    }
+
+    fn get_root(&self, i: usize) -> usize {
+        let mut k = i;
+        while !self.is_root(k) {
+            k = self.roots[k]
+        }
+        k
+    }
+
+    pub fn find(&self, i: usize) -> usize {
+        self.validate(i);
+        self.get_root(i)
+    }
+
+    pub fn union(&mut self, i: usize, j: usize) {
+        self.validate(i);
+        self.validate(j);
+        let i_root = self.get_root(i);
+        let j_root = self.get_root(j);
+
+        let (child, parent) = if self.sizes[i_root] < self.sizes[j_root] {
+            (i_root, j_root)
+        } else {
+            (j_root, i_root)
+        };
+        self.roots[child] = parent;
+        self.sizes[parent] += self.sizes[child];
+        self.count -= 1;
+    }
+
+    pub fn collapse(&mut self) {
+        for i in 0..self.roots.len() {
+            if !self.is_root(i) && !self.is_root(self.roots[i]) {
+                self.roots[i] = self.get_root(i);
+            }
+        }
+    }
+}
