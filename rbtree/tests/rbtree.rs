da34796b013847ec03c4f7da0708cfad30f50754
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-08-13 23:03:21 +0300

    Add tree tests

--- rbtree/tests/rbtree.rs
+++ rbtree/tests/rbtree.rs
@@ -6,3 +6,36 @@ fn empty() {
     assert!(tree.is_empty());
     assert_eq!(tree.len(), 0);
 }
+
+#[test]
+fn insert() {
+    let mut tree = RBTree::new();
+
+    assert_eq!(tree.insert("11".to_string(), 11), None);
+    assert!(!tree.is_empty());
+    assert_eq!(tree.len(), 1);
+    assert_eq!(tree.get("11"), Some(&11));
+    assert_eq!(tree.get("11_"), None);
+}
+
+#[test]
+fn remove() {
+    let mut tree = RBTree::new();
+    tree.insert("11".to_string(), 11);
+
+    assert_eq!(tree.remove("11"), Some(("11".to_string(), 11)));
+    assert!(tree.is_empty());
+    assert_eq!(tree.len(), 0);
+    assert_eq!(tree.remove("11"), None);
+    assert_eq!(tree.get("11"), None);
+}
+
+#[test]
+fn mutate() {
+    let mut tree = RBTree::new();
+    tree.insert("11".to_string(), 11);
+
+    *tree.get_mut("11").unwrap() += 1;
+
+    assert_eq!(tree.get("11"), Some(&12));
+}
