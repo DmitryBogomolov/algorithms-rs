19c3b5f847e4de9283ce25e2377065401a5de526
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2026-09-03 10:17:53 +0300

    Replace 'and_then' with 'map'

--- priorityqueue/src/index_priority_queue.rs
+++ priorityqueue/src/index_priority_queue.rs
@@ -96,7 +96,7 @@ where
         K: Borrow<Q>,
         Q: ?Sized + Hash + Eq,
     {
-        self.idx.get(idx).and_then(|k| self.heap.get(*k)).map(|t| (&t.0, &t.1))
+        self.idx.get(idx).map(|k| &self.heap[*k]).map(|t| (&t.0, &t.1))
     }
 
     pub fn remove(&mut self) -> Option<(K, T)> {
